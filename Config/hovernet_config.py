6dc4f202d2309e64258ef13186ce4bc6a77654ce
meihuanyuyuyu <1361577068@qq.com>
2022-09-19 13:19:07 +0000

    hovernet

--- Config/hovernet_config.py
+++ Config/hovernet_config.py
@@ -2,6 +2,8 @@ from model.HoverNet import HoverNet
 import os
 
 class Config:
+    img_size = [256,256]
+    num_classes = 7
     lr = 1e-4
     lr_s = [25]
     weight_decay = 1e-4
@@ -17,6 +19,6 @@ class Config:
         self.val_img_fp = f'figures/{self.model.__name__}_{self.__class__.__name__}'
         if not os.path.exists(self.val_img_fp):
             os.makedirs(self.val_img_fp)
-        model_para = f'model_parameters/{self.model.__name__}_{self.__class__.__name__}.pt'
+        self.model_para = f'model_parameters/{self.model.__name__}_{self.__class__.__name__}.pt'
 
 arg = Config()
